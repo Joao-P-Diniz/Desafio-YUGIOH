ea9ec6a1aa7fdfd176f3f71b7120644dd3578305
Joao Pedro <joaopedrodiniz058@gmail.com>
2025-07-14 17:47:14 -0300

    adicionando lógica nas cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -17,10 +17,78 @@ const state = {
         button:document.getElementById('next-duel'),
     }
 }
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}
 
+const pathImages = "./src/assets/icons";
+
+const cardData = [
+    {
+        id:0,
+        name: "Blue eyes white Dragon",
+        type: "paper",
+        img: `${pathImages}dragon.png` ,
+        winOf: [1],
+        loseOf: [2],
+    },
+    {
+        id:1,
+        name: "Dark Magician",
+        type: "rock",
+        img: `${pathImages}magician.png` ,
+        winOf: [2],
+        loseOf: [0],
+    },
+    {
+        id:2,
+        name: "Forbidden Exodia",
+        type: "scissor",
+        img: `${pathImages}exodia.png` ,
+        winOf: [0],
+        loseOf: [1],
+    }
+]
+
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(idCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src","./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", idCard);
+    cardImage.classList.add("card");
+
+
+    if(fieldSide === playerSides.player1){
+        cardImage.addEventListener("click", ()=>{
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    cardImage.addEventListener("mouseover",()=>{
+        drawSelectCard(idCard);
+    });
+
+    return cardImage;
+}
+
+async function drawCards(cardNumbers,fieldSide){
+    for (let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard,fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
 
 function init(){
-
+    drawCards(5,playerSides.player1);
+    drawCards(5,playerSides.computer);
 }
 
 init();
